6be292429483fcdfda5d72d6f7857746847decc7
jhanhrndz <jhancarlosh134@gmail.com>
2026-03-24 16:02:48 -0500

    Add unique constraints to username and email

--- src/main/java/com/jhan/userapi/User.java
+++ src/main/java/com/jhan/userapi/User.java
@@ -11,6 +11,10 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(unique = true, nullable = false)
     private String username;
+
+    @Column(unique = true, nullable = false)
     private String email;
 }
