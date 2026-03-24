8a99c194a7580e0241e52037d11635c8ce61591d
jhanhrndz <jhancarlosh134@gmail.com>
2026-03-24 15:57:05 -0500

    Add User entity, repository and create endpoint

---
+++ src/main/java/com/jhan/userapi/UserController.java
@@ -0,0 +1,16 @@
+package com.jhan.userapi;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+    @Autowired
+    private UserRepository userRepository;
+
+    @PostMapping
+    public User createUser(@RequestBody User user){
+        return userRepository.save(user);
+    }
+}
