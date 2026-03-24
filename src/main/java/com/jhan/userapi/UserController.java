54eba4e31789c4929ba76abf2e69c69b8c483ac8
jhanhrndz <jhancarlosh134@gmail.com>
2026-03-24 16:37:06 -0500

    Implement full CRUD for User

--- src/main/java/com/jhan/userapi/UserController.java
+++ src/main/java/com/jhan/userapi/UserController.java
@@ -3,6 +3,8 @@ package com.jhan.userapi;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/users")
 public class UserController {
@@ -12,5 +14,31 @@ public class UserController {
     @PostMapping
     public User createUser(@RequestBody User user){
         return userRepository.save(user);
+    }
+
+    @GetMapping
+    public List<User> getAllUsers(){
+        return userRepository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public User getUser(@PathVariable Long id){
+        return userRepository.findById(id).orElse(null);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteUser(@PathVariable Long id){
+        userRepository.deleteById(id);
+    }
+
+    @PutMapping("/{id}")
+    public User updateUser(@PathVariable Long id, @RequestBody User userDetails){
+        User user = userRepository.findById(id).orElse(null);
+        if (user != null){
+            user.setUsername(userDetails.getUsername());
+            user.setEmail(userDetails.getEmail());
+            return userRepository.save(user);
+        }
+        return null;
     }
 }
