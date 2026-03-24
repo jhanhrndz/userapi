2fd1d5860ade6b701386c303df230937cb7cfc66
jhanhrndz <jhancarlosh134@gmail.com>
2026-03-24 15:18:33 -0500

    init

---
+++ src/main/java/com/jhan/userapi/TestController.java
@@ -0,0 +1,13 @@
+package com.jhan.userapi;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping("/hello")
+    public String hello(){
+        return "Hello this is my first Spring Boot App";
+    }
+}
